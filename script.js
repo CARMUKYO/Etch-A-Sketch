0ad02b8939b84ab0b34a87d5a583dfdc09221d81
carmukyo <carlmike.aguila@gmail.com>
2023-06-12 15:24:01 +0800

    r

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function makeRows (rows, columns){
     container.style.setProperty('--grid-rows', rows);
     container.style.setProperty('--grid-cols', columns);
 
-    for (c = 0; c < (rows * columns); c++) {
+    for (c = 0; c < (rows * columns); c++){
         let cell = document.createElement("div");
         container.appendChild(cell).className = "grid-item";
     }
@@ -15,8 +15,7 @@ makeRows(16, 16);
 let paint = document.getElementsByClassName("grid-item");
 
 function hovered(){  
-    for (let i = 0; i < paint.length; i++)
-    {
+    for (let i = 0; i < paint.length; i++){
         paint[i].addEventListener('mouseover', function(){
             this.style.backgroundColor = "black";
         });
@@ -25,21 +24,19 @@ function hovered(){
 }
 
 function getRandomColor(){
-    var letters = '0123456789ABCDEF';
-    var color = '#';
+    let letters = '0123456789ABCDEF';
+    let color = '#';
 
-    for (var i = 0; i < 6; i++) {
+    for (let i = 0; i < 6; i++) {
       color += letters[Math.floor(Math.random() * 16)];
     }
     return color;
   }
   
 function enableMultiColor(){
-    console.log('poop');
     paint = document.getElementsByClassName("grid-item");
 
-    for (let i = 0; i < paint.length; i++)
-    {
+    for (let i = 0; i < paint.length; i++){
         paint[i].addEventListener('mouseover', function(){
             this.style.backgroundColor = getRandomColor();
         });
